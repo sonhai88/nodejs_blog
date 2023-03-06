cf9266b3bf7bb5c399e6c5cd0787e2a2c56d9fb7
sonhai88 <119028851+sonhai88@users.noreply.github.com>
2023-03-06 15:48:30 +0700

    remove public/css folder

---
+++ src/main.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require ('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', engine({
+    extname : '.hbs',
+}));
+// app.engine('handlebars', handlebars.engine({defaultLayout : 'main'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log('_dirname: ' + __dirname);
+
+//route middleware
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/:news', function (req, res) {
+    res.render('news');
+});
+
+app.listen(port, () => { console.log(`Example app listening on port localhost:${port}`)})
+
+
+// 127.0.0.1 - localhost
+
+
+//119028851+sonhai88@users.noreply.github.com
